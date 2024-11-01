4acf38c27d6af4de7bd934b463990f1c2456a8a5
Alexandria <walkerciara24@gmail.com>
2024-11-01 10:37:49 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,21 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Set up our view engine (lets us use EJS templates)
+// Set up our: view engine (lets use EJS templates)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+// This handle JSON info easily
+app.use(express.json());
+// This helps handle form submissions
+app.use(express.urlencoded({ extended: true }));
+// This allows serve static files like CSS from public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Here's home page route
+app.get('/', (req, res) => {
+    res.render('index', { 
+        title: 'Welcome!',
+        message: 'Thanks for visiting my Express app'
+    });
+});
+
