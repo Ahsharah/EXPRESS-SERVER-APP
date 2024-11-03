351a040bc9f7fd614557cca1ad7954d57d01fdbf
Alexandria <walkerciara24@gmail.com>
2024-11-03 00:15:18 -0400

    Update recipes.js

--- models/recipes.js
+++ models/recipes.js
@@ -19,3 +19,41 @@ let recipes = [
         timeInMinutes: 10
     }
 ];
+const RecipeModel = {
+    getAllRecipes: () => recipes,
+    
+    getRecipeById: (id) => recipes.find(recipe => recipe.id === parseInt(id)),
+    
+    // Get recipes by user ID
+    getRecipesByUser: (userId) => recipes.filter(recipe => recipe.userId === parseInt(userId)),
+    
+    createRecipe: (recipeData) => {
+        const newRecipe = {
+            id: recipes.length + 1,
+            ...recipeData,
+            created: new Date()
+        };
+        recipes.push(newRecipe);
+        return newRecipe;
+    },
+    updateRecipe: (id, data) => {
+        const index = recipes.findIndex(recipe => recipe.id === parseInt(id));
+        if (index !== -1) {
+            recipes[index] = { ...recipes[index], ...data };
+            return recipes[index];
+        }
+        return null;
+    },
+    
+    deleteRecipe: (id) => {
+        const index = recipes.findIndex(recipe => recipe.id === parseInt(id));
+        if (index !== -1) {
+            const deletedRecipe = recipes[index];
+            recipes = recipes.filter(recipe => recipe.id !== parseInt(id));
+            return deletedRecipe;
+        }
+        return null;
+    }
+};
+
+module.exports = RecipeModel;
