a7ec66dd03eeca7049a84098a0808169bb4f8aad
Alexandria <walkerciara24@gmail.com>
2024-11-04 13:15:19 -0500

    Update userRoutes.js

--- routes/routes/routes/userRoutes.js
+++ routes/routes/routes/userRoutes.js
@@ -1,7 +1,22 @@
-// Here's where I handle my user-related routes
+// Got my express tools ready
 const express = require('express');
 const router = express.Router();
-const UserModel = require('../models/users');
+
+// My temporary user info - I'll replace this with a database later
+let users = [
+    {
+        id: 1,
+        username: "Alexandria",
+        email: "alex@example.com",
+        favorites: ["cookies", "pasta"]
+    }
+];
+
+// Here's how I show all users
+router.get('/', (req, res) => {
+    res.json(users);
+});
+
 // Getting info for just one user
 router.get('/:id', (req, res) => {
     const user = users.find(u => u.id === parseInt(req.params.id));
@@ -24,3 +39,26 @@ router.post('/', (req, res) => {
     res.status(201).json(newUser);
 });
 
+// Updating user info
+router.put('/:id', (req, res) => {
+    const index = users.findIndex(u => u.id === parseInt(req.params.id));
+    if (index !== -1) {
+        users[index] = { ...users[index], ...req.body };
+        res.json(users[index]);
+    } else {
+        res.status(404).json({ message: "Can't find that user to update!" });
+    }
+});
+
+// Removing a user from my list
+router.delete('/:id', (req, res) => {
+    const index = users.findIndex(u => u.id === parseInt(req.params.id));
+    if (index !== -1) {
+        users = users.filter(u => u.id !== parseInt(req.params.id));
+        res.json({ message: "User deleted!" });
+    } else {
+        res.status(404).json({ message: "Can't find that user to delete!" });
+    }
+});
+
+module.exports = router;
