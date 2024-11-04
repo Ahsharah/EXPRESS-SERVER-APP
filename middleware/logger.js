12537df82ba13ea6548a0780ec58d6bf4a147e3f
Alexandria <walkerciara24@gmail.com>
2024-11-04 10:26:18 -0500

    Update logger.js

--- middleware/logger.js
+++ middleware/logger.js
@@ -3,3 +3,12 @@ const logger = (req, res, next) => {
     // I like to see the time, method, and URL for each request
     const time = new Date().toLocaleString();
     console.log(`[${time}] ${req.method}: ${req.url}`);
+        // If there's query parameters, I want to see those too
+        if (Object.keys(req.query).length > 0) {
+            console.log('Query Params:', req.query);
+        }
+        
+        next();
+    };
+    
+    module.exports = logger;
