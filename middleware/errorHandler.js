f8970dbbb7110680e8eb3595d6acac14f3e60ccc
Alexandria <walkerciara24@gmail.com>
2024-11-04 10:27:39 -0500

    Update errorHandler.js

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -2,3 +2,8 @@
 const errorHandler = (err, req, res, next) => {
     // Log the error for my reference
     console.error(`Oops! Something went wrong: ${err.message}`);
+    // I want to show different messages based on the type of error
+    const status = err.status || 500;
+    const message = status === 404 
+        ? "I couldn't find what you're looking for!" 
+        : "Something went wrong on my end!";
