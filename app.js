36ac3bbfc4512874267146e972731435973033d3
Alexandria <walkerciara24@gmail.com>
2024-11-04 13:34:50 -0500

    my page is finally visible, but need to add functionality

--- app.js
+++ app.js
@@ -1,45 +1,46 @@
-// Getting my required stuff for the server
+// Getting my tools ready
 const express = require('express');
 const path = require('path');
 const app = express();
 
-// Setting up my port - I like 3000 but this makes it flexible
+// My port setup
 const PORT = process.env.PORT || 3000;
 
-// My middleware setup - these help handle different kinds of data
+// Setting up my middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Setting up EJS - this makes my views work
+// Setting up EJS for my views
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// My home page route - keeping it simple for now
+// My home page route
 app.get('/', (req, res) => {
     res.render('index', {
-        title: 'Welcome!',
-        message: 'Hey! Welcome to my Express app!'
+        title: 'Welcome to My Recipe App!',
+        message: 'Check out my collection of awesome recipes!'
     });
 });
 
-// I'll add my other routes here once I create them
+// I'll add these routes later when I'm ready
 // const userRoutes = require('./routes/userRoutes');
+// const recipeRoutes = require('./routes/recipeRoutes');
 // const reviewRoutes = require('./routes/reviewRoutes');
 
-// And I'll use them here
 // app.use('/users', userRoutes);
+// app.use('/recipes', recipeRoutes);
 // app.use('/reviews', reviewRoutes);
 
-// This handles pages that don't exist
-app.use((req, res, next) => {
+// Handling 404s
+app.use((req, res) => {
     res.status(404).render('error', {
-        message: "Oops! I couldn't find that page!",
+        message: "Oops! Can't find that page!",
         error: { status: 404 }
     });
 });
 
-// Starting up my server!
+// Starting my server
 app.listen(PORT, () => {
     console.log(`Awesome! My server is up on port ${PORT}`);
 });
